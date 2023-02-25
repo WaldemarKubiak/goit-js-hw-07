ac6f53734664d85212113bb7c3e5efd0385fe0e2
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-02-25 15:27:46 +0100

    Task-2 finished: pictures gallery with using library SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryArrRef = document.querySelector(".gallery");
+const picturesMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a>`
+  )
+  .join("");
+
+galleryArrRef.insertAdjacentHTML("afterbegin", picturesMarkup);
+
+// SimpleLightbox library use
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionType: "alt",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+// console.log(galleryItems);
+// console.log(galleryArrRef);
